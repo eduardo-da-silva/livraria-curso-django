be77deaa2d16ef8cf67cd8a1fe4ddafe54c5899c
Eduardo da Silva <eduardo.silva@gmail.com>
2021-11-16 15:53:37 -0300

    Adiciona Swagger e Redoc

--- livraria/urls.py
+++ livraria/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 from rest_framework import routers
 
 from core import views
@@ -14,6 +19,14 @@ router.register(r"compras", views.CompraViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    # OpenAPI 3
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    # Autenticação
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    # Outros endpoints
     path("teste/", views.teste),
     path("pag2/", views.teste2),
     path("categorias-class/", views.CategoriaView.as_view()),
@@ -22,5 +35,5 @@ urlpatterns = [
     path("categorias-apiview/<int:id>/", views.CategoriaDetail.as_view()),
     path("categorias-generic/", views.CategroriasListGeneric.as_view()),
     path("categorias-generic/<int:id>/", views.CategoriaDetailGeneric.as_view()),
-    path("", include(router.urls)),
+    path("api/", include(router.urls)),
 ]
